bf12e7c880ab2a9d5f1a5a5e438b07cca787d2b8
Diego <diegolima.almeida93@gmail.com>
2022-11-30 15:48:28 -0300

    feat: create globalStyle/typhography

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+import { GlobalStyle } from './styles/global';
+import { Typography } from './styles/typography';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from './App';
@@ -5,6 +7,8 @@ import { App } from './App';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <GlobalStyle />
+    <Typography />
     <App />
   </React.StrictMode>
 );
