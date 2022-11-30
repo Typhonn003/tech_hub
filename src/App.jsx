6617cdbed795939ee7aab7578c8af26710a2b6e7
Diego <diegolima.almeida93@gmail.com>
2022-11-30 20:54:06 -0300

    delete: app.jsx codes

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,1 @@
-import { PrimaryButton } from "./components/Button/Default";
-import { Input } from "./components/Input/Default";
-import { Select } from "./components/Input/Select";
-
-export function App() {
-
-    return (
-    <div>
-        <PrimaryButton>Oi</PrimaryButton>
-        <Input placeholder="Teste" type="text" />
-        <label htmlFor="123">Teste</label>
-        <Select id="123">
-            <option value="">Teste</option>
-            <option value="">Teste</option>
-        </Select>
-    </div>
-    )
-}
+export function App() {}
