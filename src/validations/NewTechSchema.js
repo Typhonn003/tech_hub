b758dcb15660c7e91858d070c0d004463e7f4335
Diego <diegolima.almeida93@gmail.com>
2023-08-06 14:38:25 -0300

    refactoring: create vite project

--- src/validations/NewTechSchema.js
+++
@@ -1,8 +0,0 @@
-import * as yup from "yup";
-
-export const newTechSchema = yup.object().shape({
-  title: yup
-    .string()
-    .required("Nome obrigatório*")
-    .max(16, "Nome máximo de 16 caracteres"),
-});
